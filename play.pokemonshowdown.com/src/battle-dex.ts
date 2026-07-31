eef43aa37d9fcd188a16ba3f474ba3e8a8e392c2
ItzGray <masonmustonen@gmail.com>
2026-07-30 20:29:47 -0500

    Fixed Luca Zamon and Tea Barqan

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -779,6 +779,8 @@ export const Dex = new class implements ModdedDex {
 			num = BattlePokedex[id].num;
 		}
 		if (num < 0) num = 0;
+		if (id === "lucazamn") num = 5382;
+		if (id === "tabarqn") num = 5383;
 
 		if (window.BattlePokemonIconIndexes?.[id]) {
 			num = BattlePokemonIconIndexes[id];
