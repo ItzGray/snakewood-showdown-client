7f54838c09d1d415713c848f7aec480cd82ca604
ItzGray <masonmustonen@gmail.com>
2026-07-31 02:17:06 -0500

    Fixed 1 base stat pokemon showing as 1HP

--- play.pokemonshowdown.com/src/oldclient/client-teambuilder.js
+++ play.pokemonshowdown.com/src/oldclient/client-teambuilder.js
@@ -3679,7 +3679,6 @@
 			if (ev === undefined) ev = (this.curTeam.gen > 2 ? 0 : 252);
 
 			if (stat === 'hp') {
-				if (baseStat === 1) return 1;
 				if (usesStatPoints) return baseStat + ev + 75;
 				if (!supportsEVs) return Math.floor(Math.floor(2 * baseStat + iv + 100) * set.level / 100 + 10) + (supportsAVs ? ev : 0);
 				return Math.floor(Math.floor(2 * baseStat + iv + Math.floor(ev / 4) + 100) * set.level / 100 + 10);
