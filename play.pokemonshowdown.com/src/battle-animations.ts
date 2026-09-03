7e47c66ad9a261f680ec0cccfa214d9af0195f67
ItzGray <masonmustonen@gmail.com>
2026-09-03 14:20:41 -0500

    Made it so replay effects should always work

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -288,7 +288,7 @@ export class BattleScene implements BattleSceneStub {
 	) {
 		if (typeof effect === 'string') effect = BattleEffects[effect];
 
-		let $effect = $(`<img src="${effect.url!}" style="display:block;position:absolute" />`);
+		let $effect = $(`<img src="https://play.pokemonshowdown.com/fx/${effect.url!}" style="display:block;position:absolute" />`);
 		this.$fx.append($effect);
 		if (additionalCss) $effect.css(additionalCss);
 		$effect = this.$fx.children().last();
